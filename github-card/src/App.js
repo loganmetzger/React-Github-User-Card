9172172701bc563a37e96e5757f5b4a7ee93b12f
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-08-04 18:25:54 -0400

    created some components and got data from axios call

--- github-card/src/App.js
+++ github-card/src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      gitFriends: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/loganmetzger`)
+      .then(res  => {
+        this.setState({ gitFriends: res.data })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Git Frands</h1>
+        <CardList />
+      </div>
+    )
+  }
 }
 
 export default App;
